c7c673b11fba89e201ef1553c55eccf875ad2459
umgonji <umgonji@naver.com>
2020-06-21 14:14:55 +0900

    내가 말한 내용 (버튼 누르면) 실제 채팅으로 등록

--- app/src/main/java/dasdsa/sdn/numberbaseballgame_20200621/MainActivity.kt
+++ app/src/main/java/dasdsa/sdn/numberbaseballgame_20200621/MainActivity.kt
@@ -2,6 +2,7 @@ package dasdsa.sdn.numberbaseballgame_20200621
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import dasdsa.sdn.numberbaseballgame_20200621.adapters.ChatAdapter
 import dasdsa.sdn.numberbaseballgame_20200621.datas.Chat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -20,6 +21,27 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        inputBtn.setOnClickListener {
+            // 사용자 입력값을 받아오자.
+            val inputNum = numberEdt.text.toString()
+
+            //세자리 일때만 OK
+            if(inputNum.length != 3) {
+                Toast.makeText(mContext, "세자리 숫자만 입력 가능합니다.", Toast.LENGTH_SHORT).show()
+                //이벤트 강제종료
+                 return@setOnClickListener
+            }
+
+            //입력값이 3자리임이 보증됨.
+            //사용자가 한 말로 입력 처리 => 채팅 내역으로 추가
+
+            chatList.add(Chat("ME", inputNum))
+
+            mChatAdapter.notifyDataSetChanged()
+
+
+        }
 
     }
 
