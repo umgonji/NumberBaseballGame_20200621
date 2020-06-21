2c8fe36ccf2445a7c6b393bc951d52069d265c8a
umgonji <umgonji@naver.com>
2020-06-21 15:12:26 +0900

    주석없앤거변

--- app/src/main/java/dasdsa/sdn/numberbaseballgame_20200621/MainActivity.kt
+++ app/src/main/java/dasdsa/sdn/numberbaseballgame_20200621/MainActivity.kt
@@ -11,7 +11,6 @@ import kotlin.math.log
 
 class MainActivity : BaseActivity() {
 
-    //문자로 나온 숫자를 담는 배열
     val cpuNumList = ArrayList<Int>()
 
     val chatList = ArrayList<Chat>()
@@ -28,60 +27,45 @@ class MainActivity : BaseActivity() {
     override fun setupEvents() {
 
         inputBtn.setOnClickListener {
-            // 사용자 입력값을 받아오자.
             val inputNum = numberEdt.text.toString()
 
-            //세자리 일때만 OK
             if(inputNum.length != 3) {
                 Toast.makeText(mContext, "세자리 숫자만 입력 가능합니다.", Toast.LENGTH_SHORT).show()
                 //이벤트 강제종료
                  return@setOnClickListener
             }
 
-            //입력값이 3자리임이 보증됨.
-            //사용자가 한 말로 입력 처리 => 채팅 내역으로 추가
 
             chatList.add(Chat("ME", inputNum))
 
-            //한번 숫자 입력시 입력칸 비워주기.
             numberEdt.setText("")
 
-            //입력한 값을 가지고 ?S ?B인지 판단해서 답장하기.
             CheckUserInputStrikeAndBall(inputNum)
         }
     }
 
     fun CheckUserInputStrikeAndBall(input : String) {
 
-        //input : String 을 Int로 변경하자.
         val number = input.toInt() //"256" => 256
-
-        //세자리 숫자를 => 세칸의 배열로 분리. 256 => 2, 5, 6
         val numArr = ArrayList<Int>()
         numArr.add( number / 100)   //100의자리 2 : 256/100 = 2.56
         numArr.add( number/10 % 10)    //10의자리 5
         numArr.add( number % 10)    //1의자리 6
 
-        //찾은  S / B 갯수를 저장할 변수
         var strikeCount = 0
         var ballCount = 0
 
-        //내 입력가븡ㄹ 돌아주는 반복 : i가 내 숫자들을 담당
+
         for ( i in numArr.indices) {
 
-            //문제 숫자를 돌아주는 반복 : j 가 컴퓨터 숫자들 담당
             for ( j in cpuNumList.indices) {
 
-                //내 숫자와 cpu 숫자가 같은가?
                 if (numArr[i] == cpuNumList[j] ){
-                    //위치도 같은가?
                     if ( i == j )
                     {
-                        //같다면 S 하나 더 찾았다.
                         strikeCount ++
                     }
                     else {
-                        //숫자는 같지만, 위치가 다르면 B 하나더 찾았다.
                         ballCount ++
                     }
                 }
@@ -89,8 +73,6 @@ class MainActivity : BaseActivity() {
             }
         }
 
-        // ?S, ?B 판단이 모두 끝남.
-        //결과를 cpu가 답장
 
         chatList.add(Chat("CPU", "${strikeCount}S ${ballCount}B 입니다."))
 
